582a2f3c40fab584dc91e3f4028c9bf434aeed92
Ronel1409 <ronelmichael14@gmail.com>
2023-04-04 16:24:57 -0700

    new

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,7 @@ const Navbar = () => {
                         {/* <Link className="link" to={"/assets/vendor/SearchPage"}>
                             <input style={{fontSize: '19px',  fontWeight: '300', color: 'black'}} className="pa2 ph4 code br-pill ba" type="search" placeholder="Search products...."/>          
                         </Link> */}
-                        <Link to={""} title="feed" className="ph3 fw5 link black" ></Link>
+                        <Link to={""} title="feed" className="ph4 fw5 link black" ></Link>
                         <Link to={"../assets/vendor/SearchPage"} title="feed" className="navbar pv2 f3 hover-bg-light-gray br3 icon-magnifier ph4 fw5 link black" ></Link>
                         <Link style={{justifyContent: 'flex-end'}} to={"/assets/vendor/Profile"} className="navbar pt2 pointer fw5 br3 pa2 ph4 f4">
                             <svg width="22px" height="22px" viewBox="0 0 16 16" className="black bi bi-person-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
