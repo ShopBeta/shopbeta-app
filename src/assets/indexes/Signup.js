e80d4f1ac68009d6531363944eb9addadeb46613
Ronel1409 <ronelmichael14@gmail.com>
2023-04-13 17:01:13 -0700

    updates

--- src/assets/indexes/Signup.js
+++ src/assets/indexes/Signup.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import Preloader from "../../components/Preloader";
 
 
-const Signup = () => {
+const Signup = ({username, email, password, phonenumber}) => {
 
 const newUser = async () => {
 
@@ -50,26 +50,26 @@ console.log(me)
                         <div className="pv3">
                             <div className='tc code' style={{fontSize: '27px', fontWeight: '500'}}><img src={img} alt="logo" className="tc" width="75px" height="75px" /></div>
                             <p className="pv2 fw6 tc"><small className="orange f4">Welcome</small><br/> It only takes a <small className="green f4">few seconds</small> to create your account</p>
-                                <form onSubmit={handleSubmit}>
+                                <form action={"https://shopbeta-api.onrender.com/users"} method="post" encType="multipart/form-data">
                                     <div className="tl pv2">
                                         <p className="pv1 f5 fw5">Username</p>
                                         <small className="icon-user f4 fw5"></small>
-                                        <input type="text" name="username" className="username br3 ba pa3 w-100" placeholder="Type in username..." required />
+                                        <input type="text" name="username" value={username} className="username br3 ba pa3 w-100" placeholder="Type in username..." required />
                                     </div>
                                     <div className="tl pv2">
                                         <p className="pv1 f5 fw5">Phone Number</p>
                                         <small className="icon-phone f4 fw5"></small>
-                                        <input type="tel" name="phonenumber" className="phonenumber br3 ba pa3 w-100" placeholder="Input phone number..." required />
+                                        <input type="tel" name="phonenumber" value={phonenumber} className="phonenumber br3 ba pa3 w-100" placeholder="Input phone number..." required />
                                     </div>
                                     <div className="tl pv2">
                                         <p className="pv1 f5 fw5">Email</p>
                                         <small className="icon-envelope f4 fw5"></small>
-                                        <input type="email" name="email" className="email br3 ba pa3 w-100" placeholder="Your email..." required autoComplete="email" />
+                                        <input type="email" name="email" value={email} className="email br3 ba pa3 w-100" placeholder="Your email..." required autoComplete="email" />
                                     </div>
                                     <div className="tl pv2">
                                         <p className="pv1 f5 fw5">Password</p>
                                         <small className="icon-lock f4 fw5"></small>
-                                        <input type="password" name="password" className="password br3 ba pa3 w-100" placeholder="password..." required autoComplete="current-password" />
+                                        <input type="password" value={password} name="password" className="password br3 ba pa3 w-100" placeholder="password..." required autoComplete="current-password" />
                                     </div>
                                     <div className="tl pv2">
                                         <input name="check" id="exampleCheck" type="checkbox" className="pr2" required /><label for="exampleCheck" className="form-check-label ph2">Accept our <a href="https://shopbetaonline.onrender.com/terms of use.html">Terms and Conditions</a>.</label>
